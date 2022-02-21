9f5a8524d4cff550fe27feae21efedb622c8e01e
vitorcape <95989293+vitorcape@users.noreply.github.com>
2022-02-21 12:57:29 -0300

    Update script.js

--- dip/script.js
+++ dip/script.js
@@ -3,10 +3,12 @@ $(".header_toggle").click(function () {
         $("#body-pd").removeClass("body-pd");
         $("header").removeClass("body-pd");
         $("#nav-bar").removeClass("show");
+        $("#header-toggle").removeClass("bx-x");
     }
     else{
         $("body").addClass("body-pd");
         $("header").addClass("body-pd");
         $("#nav-bar").addClass("show");
+        $("#header-toggle").addClass("bx-x");
     };
  });
