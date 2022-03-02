d591da9d32c413a2dd0d45d44d96cc701fb48ab1
vitorcape <95989293+vitorcape@users.noreply.github.com>
2022-03-01 21:28:41 -0300

    Update script.js

--- forum/script.js
+++ forum/script.js
@@ -38,3 +38,5 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+$('select[name="usergroup"]').addClass('form-select');
