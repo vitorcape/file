772d90b7b46cec4b70a6f7aef9b66f65886882ab
vitorcape <95989293+vitorcape@users.noreply.github.com>
2023-12-13 10:38:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
       var $this = $(this);
       var username = $this.find('#header-user span').text();
       var $img1 = $this.find('#header-user img');
-      var $img2 = $this.find('.avatar-big img');
+      var $img2 = $this.find('#main-user-menu .avatar-big img');
 
       $img1.attr('src', 'https://imager.radiohabblet.com.br/?user=' + username + '&action=std,crr=&gesture=sml&direction=4&head_direction=3&headonly=true&size=l&img_format=&dance=&effect=&frame_num=30');
       $img2.attr('src', 'https://imager.radiohabblet.com.br/?user=' + username + '&direction=4&head_direction=3&size=l&gesture=sml&action=std');
