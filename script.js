54d8f1ae322242d5f4c668a417ee03077ed54ec1
vitorcape <95989293+vitorcape@users.noreply.github.com>
2023-12-13 11:04:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
       var $img1 = $this.find('#header-user img');
       var $img2 = $this.find('#main-user-menu .avatar-big img');
 
-      $img1.attr('src', 'https://imager.radiohabblet.com.br/?user=' + username + '&action=std,crr=&gesture=sml&direction=4&head_direction=3&headonly=true&size=l&img_format=&dance=&effect=&frame_num=30');
+      $img1.attr('src', 'https://imager.radiohabblet.com.br/?user=' + username + '&action=std,crr=&gesture=sml&direction=4&head_direction=3&headonly=true&size=m&img_format=&dance=&effect=&frame_num=30');
       $img2.attr('src', 'https://imager.radiohabblet.com.br/?user=' + username + '&direction=4&head_direction=3&size=l&gesture=sml&action=std');
     });
 }(jQuery));
