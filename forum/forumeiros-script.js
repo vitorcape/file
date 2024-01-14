f4f93759ee813b6bd15d2c7d2d96a79d6b3e62d4
vitorcape <95989293+vitorcape@users.noreply.github.com>
2024-01-14 11:17:11 -0300

    Create forumeiros-script.js

---
+++ forum/forumeiros-script.js
@@ -0,0 +1,7 @@
+/* ICONE USUARIO MENU */
+
+$(function() {
+'use strict';
+
+$('#user-avatar-icon').append("<img src='https://imager.radiohabblet.com.br/?user=" + _userdata.username + "&action=std,crr=&gesture=std&direction=4&head_direction=3&headonly=true&size=b&img_format=&dance=&effect=&frame_num=30'/>");
+});
